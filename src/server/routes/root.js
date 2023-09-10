ef8385884bff07f3a46e76b73b727b477e0cfbb5
exoup <JosephY@dram.space>
2023-09-09 23:36:23 -0500

    Initial root route

--- src/server/routes/root.js
+++ src/server/routes/root.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.get('/', (req, res, next) => {
     res.render('index', {
-        example: 'example'
+        title: 'Hello, example'
     });
 });
 
