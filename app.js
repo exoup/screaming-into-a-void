ec3dbc3b46973c9388991b93bdb44d8b9fea5afa
Joseph Y <josephy@dram.space>
2023-09-19 23:37:20 -0500

    Remove some unused imports

--- app.js
+++ app.js
@@ -3,24 +3,20 @@
 //Dependencies
 const express = require('express');
 const path = require("path");
-const public = path.join(__dirname, './src/client/public');
+const public = path.join(__dirname, './dist');
 
 //Using
 const app = express();
 app.set('trust proxy', 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './src/client/views'));
 
 
 //Routes
-const root = require('./src/server/routes/root');
 const events = require('./src/server/routes/events');
 
 //Use routes
 app.use(express.static(public));
-app.use('/', root);
 app.use('/events', events);
 app.get('*', (req, res) => {
     res.status(404).send("Your page is pining for the fjords.");
