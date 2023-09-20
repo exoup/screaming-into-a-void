6dbf89071b750a42a04197f24a5d401d92ad0033
Joseph Y <josephy@dram.space>
2023-09-19 23:37:51 -0500

    Unuse

--- src/server/routes/root.js
+++ src/server/routes/root.js
@@ -2,9 +2,6 @@ const router = require("express").Router();
 
 router.get('/', (req, res, next) => {
     try {
-        res.render('index', {
-            title: 'Hello, example'
-        });
     }
     catch (e) {
         next(e);
