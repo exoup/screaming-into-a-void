7af8c19cd0b2222fc65e8c89d708b6f0475ab466
Joseph Y <josephy@dram.space>
2023-09-21 00:03:40 -0500

    Add some test routing component

---
+++ src/client/components/VoidAbout.jsx
@@ -0,0 +1,21 @@
+import { Component, h, Fragment } from 'preact';
+
+// export default class VoidHome extends Component {
+//     render() {
+//         return (
+//             <>
+//                 <p>Hello, voidy.</p>
+//             </>
+//         )
+//     }
+// };
+
+export default function VoidHome() {
+    return (
+        <>
+            <p>
+                About, voidy.
+            </p>
+        </>
+    )
+}
